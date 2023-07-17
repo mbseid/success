a70169582b30aa483735f368659991ba50e7a5b6
Mike Seid <mbseid@gmail.com>
2023-07-16 22:40:24 -0400

    disabling graphql cache. Do things that don't scale

--- frontend/app/graphql.js
+++ frontend/app/graphql.js
@@ -3,7 +3,17 @@ import { ApolloClient, gql as apolloGQL, InMemoryCache } from "@apollo/client";
 export const graphQLClient = new ApolloClient({
     cache: new InMemoryCache(),
     ssrMode: true,
-    uri: `${process.env.API_HOST}/graphql`
+    uri: `${process.env.API_HOST}/graphql`,
+    defaultOptions: {
+        watchQuery: {
+          fetchPolicy: 'no-cache',
+          errorPolicy: 'all',
+        },
+        query: {
+          fetchPolicy: 'no-cache',
+          errorPolicy: 'all',
+        },
+      }
 });
 
 export const gql = apolloGQL;
