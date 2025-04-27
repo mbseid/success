f1a84298e653cd3ade34384281651f0e8b722c66
Mike Seid <mbseid@gmail.com>
2025-04-27 13:03:38 -0400

    Removing the CSRF protectino as the calls are only coming from the redux frontend

--- backend/success/settings.py
+++ backend/success/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware', # Commenting this out because there is no requests coming from the client.
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
